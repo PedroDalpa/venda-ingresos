4286f4d5ad56990214e0223c0c71c528e1487d5f
PedroDalpa <pedro.dalpa@gmail.com>
2024-06-18 09:15:19 -0300

    Create CRUD in spots service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
-  imports: [EventsModule, PrismaModule],
+  imports: [EventsModule, PrismaModule, SpotsModule],
   controllers: [],
   providers: [],
 })
