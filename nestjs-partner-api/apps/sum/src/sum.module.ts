54795cf6cc986a1706e53a197beb1a731eb8efe8
PedroDalpa <pedro.dalpa@gmail.com>
2024-06-18 17:57:07 -0300

    Make sum partner run with another database called sum

--- nestjs-partner-api/apps/sum/src/sum.module.ts
+++ nestjs-partner-api/apps/sum/src/sum.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../../../libs/core/src/prisma/prisma.module';
 
 @Module({
-  imports: [EventsModule, SpotsModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.sum', isGlobal: true }),
+    PrismaModule,
+    EventsModule,
+    SpotsModule,
+  ],
 })
 export class SumModule {}
--- nestjs-partner-api/libs/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/events/events-core.module';
-export * from './src/events/events-core.service';
-export * from './src/spots/spots-core.module';
-export * from './src/spots/spots-core.service';
