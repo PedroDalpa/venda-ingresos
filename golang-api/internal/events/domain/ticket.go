dcb8ad80d7e21b98e9ffed2809b9dee620cbc41b
PedroDalpa <pedro.dalpa@gmail.com>
2024-06-19 11:06:42 -0300

    Create a ticket validation in go api

--- golang-api/internal/events/domain/ticket.go
+++ golang-api/internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "fmt"
+
 type TicketType string
+
+var (
+	ErrTicketPriceZero = fmt.Errorf("ticket price must be greater than or equal to zero")
+)
 
 const (
 	TicketTypeHalf TicketType = "half"
@@ -13,4 +19,21 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func isValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price < 0 && isValidTicketType(t.TicketType) {
+		return ErrTicketPriceZero
+	}
+	return nil
 }
